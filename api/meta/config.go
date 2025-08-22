fb92e411dc9c6ae87cd5f0a32753af2c31ec8f4c
Sergei Lukianov <me@slukjanov.name>
2025-08-21 20:28:19 -0700

    chore(o11r): drop more unused config opts
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- api/meta/config.go
+++ api/meta/config.go
@@ -15,9 +15,6 @@ type GatewayCtrlConfig struct {
 	FRRRef               string              `json:"frrRef,omitempty"`
 	DataplaneMetricsPort uint16              `json:"dataplaneMetricsPort,omitempty"`
 	FRRMetricsPort       uint16              `json:"frrMetricsPort,omitempty"`
-	RegistryURL          string              `json:"registryURL,omitempty"`
-	RegistryCASecret     string              `json:"registryCACertSecret,omitempty"`
-	RegistryAuthSecret   string              `json:"registryAuthSecret,omitempty"`
 }
 
 type AgentConfig struct {
